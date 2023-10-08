82f8518d31f51226a5630a463684ffe71e13dcee
Haseebismail703 <haseebismail703@gmail.com>
2023-10-08 22:27:09 +0500

    half

---
+++ class 1/app.js
@@ -0,0 +1,61 @@
+
+// spread operator
+
+// let arr = [234,234,234,34,234,234]
+// let arr2 = [34,343,234,34,34,3]
+
+// let arr3 = [...arr, ...arr2]
+// document.write(arr3)
+
+// let arr =[324,234,234,324,34]
+// let arr1 = [...arr,123,123,123,123]
+// document.write(arr1)
+
+
+// let  obj = {
+//     first : 'haseeb',
+//     second :  'ismial'
+// }
+
+// let obj1 = {
+//     ...obj,
+//     iname : 'kamal'
+// }
+// console.log(obj1)
+
+
+// expression function
+
+// let foo = function (){
+//     console.log('haseeb')
+// }
+// foo()
+
+
+// aero function 
+
+// let abc = () => {
+//     console.log('Haseeb')
+// }
+
+// abc()
+
+
+// let sum = (a,b) => {
+//     return a+ b
+// }
+
+// console.log(sum(2,3))
+
+// shortkut  aero function 
+
+
+// let sum = (a,b) => a,b
+
+// console.log(sum(2+2))
+
+
+//  Ternary operator
+
+// let abc = ( 5==5 ? 'han sahi hai':'nhi hai')
+// console.log(abc)
