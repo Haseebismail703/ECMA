ef307c1928a45700970e58148b9ae7a0bd5ff722
Haseebismail703 <haseebismail703@gmail.com>
2023-12-12 12:39:35 +0500

    ecma

---
+++ sir ghouse/app.js
@@ -0,0 +1,114 @@
+
+    //  var let const   function ka ander bna hoga to function ka  bahir nhi chala ga
+    // sab mai abc not define aiga 
+
+// example 1 
+
+// function foo(){
+//    var  abc  ='Haseeb'
+// }
+// console.log(abc)
+ 
+
+// function foo(){
+//     let   abc  ='Haseeb'
+//  }
+//  console.log(abc)
+
+
+//  function foo(){
+//     const  abc  ='Haseeb'
+//  }
+//  console.log(abc)
+
+
+//  agar if ki condition lagi hogi to var chala ga let or const nhi 
+//  or false hoga to Undefine aiga 
+
+// if(true){
+//    var abc = 'Haseeb'
+// }
+// console.log(abc);  // Haseeb
+
+
+ 
+// var let  ko bad mai value assighn kar sakta hai const mai nhi 
+
+
+
+// var abc = 'Haseeb'
+// abc = 'Rehman'
+// console.log(abc);  // Rehman
+
+// let abc = 'Haseeb'
+// abc = 'Rehman'
+// console.log(abc);  // Rehman
+
+
+// const abc = 'Haseeb'
+// abc = 'Rehman'
+// console.log(abc) 
+
+// Uncaught TypeError: Assignment to constant variable.
+
+// ak jasa name bhi nhi dasakta 
+
+
+// var  abc = 'Haseeb'
+// console.log(abc) 
+
+// const abc = 'Haseeb'
+// console.log(abc) 
+//  abc alredy decleare 
+
+
+// var  abc = 'Haseeb'
+// console.log(abc) 
+
+// let abc = 'Haseeb'
+// console.log(abc) 
+
+
+
+
+
+// template literals
+
+
+// var abc= 'Haseeb'
+// var abs = `rehman
+//  raheem`
+
+// var ful = `${abc} ${abs}`
+// console.log(ful);
+
+
+// function foo(...rest ){
+
+//     console.log(rest[1])
+// }
+// foo(1,2,3,4,5,6,7)
+
+
+
+
+
+// let obj = {
+//     name : 'Haseeb'
+// }
+
+// let obj1 = obj
+// obj1.name = 'Rehman'
+// console.log(obj1);
+// console.log(obj);
+
+
+
+let obj = {
+    name : 'Haseeb'
+}
+
+let obj1 = {...obj}
+obj1.name = 'Rehman'
+console.log(obj);
+console.log(obj1);
